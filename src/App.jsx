0980b4dbfd466c153351dc8c54bab395d62ad74d
Raj Aryan <raj.aryan2518@gmail.com>
2026-05-23 16:24:32 +0530

    completed the background compo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Background from './components/Background'
+import Forground from './components/Forground'
 
 function App() {
 
@@ -8,7 +9,9 @@ function App() {
     <>
       <div className=' relative bg-zinc-800 w-full h-screen'>
     <Background/>
-       
+        <div className='fixed top-0 left-0 w-full h-full   z-10'>
+
+        </div>
       </div>
       
       
--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -3,10 +3,13 @@ import React from 'react'
 export default function Background() {
   return (
       <>
+          <div className='w-full h-screen fixed z-2'>
+              
           <div className=' absolute top-[5%] text-zinc-300 text-xl flex justify-center py-10 w-full'>
-              Documents
+              @beingrax
           </div>
-          <h1 className=' absolute text-[13vw] leading-none tracking-tight top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] text-zinc-400 font-semibold'>Docs.</h1>
+          <h1 className=' absolute text-[13vw] leading-none tracking-tight top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-zinc-400 font-semibold'>Docs.</h1>
+          </div>
       </>
   )
 }
