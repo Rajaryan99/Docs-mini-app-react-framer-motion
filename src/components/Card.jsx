7672e15c684a2f257b8461c744b0fe54dadd0a12
Raj Aryan <raj.aryan2518@gmail.com>
2026-05-24 16:11:21 +0530

    added a card componnets

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Card() {
+  return (
+      <>
+          <div className='w-60 h-72 rounded-4xl  bg-zinc-600'>
+
+              
+
+          </div>
+
+      </>
+  )
+}
