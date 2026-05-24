09066a37467086d4d061ac6fe21c32fa3e5698c1
Raj Aryan <raj.aryan2518@gmail.com>
2026-05-24 16:25:48 +0530

    designed card compo with tailkwindcss

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { FaRegFileAlt } from "react-icons/fa";
+
 
 export default function Card() {
   return (
       <>
-          <div className='w-60 h-72 rounded-4xl  bg-zinc-600'>
-
-              
+          <div className=' relative overflow-hidden w-52 h-65 py-10 px-5 text-white  rounded-4xl  bg-zinc-900/90'>
+              <FaRegFileAlt />
+              <p className='mt-3  text-[15px] font-semibold leading-tight'>Lorem ipsum dolor sit amet consectetur adipisicing. Lorem ipsum dolor sit.</p>
+              <div className='footer absolute w-60 left-0 bottom-0 h-13 bg-blue-100'>
+                  
+              </div>
 
           </div>
 
--- src/components/Forground.jsx
+++ src/components/Forground.jsx
@@ -4,7 +4,7 @@ import Card from './Card'
 export default function Forground() {
     return (
       <>
-        <div className='fixed top-0 left-0 w-full h-full   z-10'>
+        <div className='fixed top-0 left-0 w-full h-full p-5   z-10'>
           
           <Card/>
             </div>
