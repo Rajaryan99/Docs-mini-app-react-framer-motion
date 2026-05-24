ae2442d982a358f12a745ce04e4fd8ce664705e3
Raj Aryan <raj.aryan2518@gmail.com>
2026-05-24 20:45:42 +0530

    added card functionality

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { FaRegFileAlt } from "react-icons/fa";
+import { FaDownload } from "react-icons/fa6";
+
 
 
 export default function Card() {
@@ -8,7 +10,15 @@ export default function Card() {
           <div className=' relative overflow-hidden w-52 h-65 py-10 px-5 text-white  rounded-4xl  bg-zinc-900/90'>
               <FaRegFileAlt />
               <p className='mt-3  text-[15px] font-semibold leading-tight'>Lorem ipsum dolor sit amet consectetur adipisicing. Lorem ipsum dolor sit.</p>
-              <div className='footer absolute w-60 left-0 bottom-0 h-13 bg-blue-100'>
+              <div className='footer absolute w-full left-0 bottom-0 '>
+                  
+                  <div className='flex justify-between items-center px-8 text-sm mb-3'>  
+                      <h5>.4mb</h5>
+                      <FaDownload/>
+                  </div>
+                  <div className='tag left-0 w-full py-3 flex justify-center items-center bg-green-600'>
+                      <h3 className='font-semibold text-md'>Download Now</h3>
+                  </div>
                   
               </div>
 
