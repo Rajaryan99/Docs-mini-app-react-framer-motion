6bfcce748651451dbad684129aeecf82e8b33969
Raj Aryan <raj.aryan2518@gmail.com>
2026-05-24 21:35:06 +0530

    added a dynamic data using jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,32 @@
 import React from 'react'
 import { FaRegFileAlt } from "react-icons/fa";
 import { FaDownload } from "react-icons/fa6";
+import { IoClose } from "react-icons/io5";
 
 
 
-export default function Card() {
+export default function Card({data}) {
   return (
       <>
-          <div className=' relative overflow-hidden w-52 h-65 py-10 px-5 text-white  rounded-4xl  bg-zinc-900/90'>
+          <div className=' relative overflow-hidden w-52 h-65 py-10 px-5  text-white  rounded-4xl  bg-zinc-900/90'>
               <FaRegFileAlt />
-              <p className='mt-3  text-[15px] font-semibold leading-tight'>Lorem ipsum dolor sit amet consectetur adipisicing. Lorem ipsum dolor sit.</p>
+              <p className='mt-3  text-[15px] font-semibold leading-tight'>{data.desc}</p>
               <div className='footer absolute w-full left-0 bottom-0 '>
                   
                   <div className='flex justify-between items-center px-8 text-sm mb-3'>  
-                      <h5>.4mb</h5>
-                      <FaDownload/>
+                      <h5>{data.filesize}</h5>
+                      <span className='text-l bg-zinc-600 rounded-full p-[4px]'>
+                          {data.close ? <IoClose /> : <FaDownload />}
+
+                      </span>
                   </div>
-                  <div className='tag left-0 w-full py-3 flex justify-center items-center bg-green-600'>
-                      <h3 className='font-semibold text-md'>Download Now</h3>
-                  </div>
+
+                  {data.tag.isOpen ? (
+                      <div className='tag left-0 w-full py-3 flex justify-center items-center bg-green-600'>
+                          <h3 className='font-semibold text-md'>{data.tag.tagTitle}</h3>
+                      </div>
+                  ) : null}
+                  
                   
               </div>
 
--- src/components/Forground.jsx
+++ src/components/Forground.jsx
@@ -2,11 +2,25 @@ import React from 'react'
 import Card from './Card'
 
 export default function Forground() {
+
+  const data = [
+    {
+      desc: " This is Docs An AI basied not taking application, with some very cool design.",
+      filesize: ".9mb",
+      close: true,
+      tag : {isOpen: false, tagTitle: "Download Now", tagColor: "green"}
+    },
+  ]
+
     return (
       <>
-        <div className='fixed top-0 left-0 w-full h-full p-5   z-10'>
+        <div className='fixed top-0 left-0 w-full h-full p-5    z-10'>
           
-          <Card/>
+          {data.map((item, index) => (
+            <Card data={item} />
+          ))}
+          
+          
             </div>
         </>
   )
